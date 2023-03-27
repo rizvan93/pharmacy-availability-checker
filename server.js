60bf13c96e16d1e00aaf7530425ab4cb53c9bf45
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-03-27 21:47:18 +0800

    add routes for server as comments

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ const logger = require("morgan");
 require("dotenv").config();
 require("./config/database");
 
-const app = express();
+//const usersRouter = require("./routes/usersRouter")
+//const pharmacistsRouter = require("./routes/pharmacistsRouter")
+//const consumersRouter = require("./routes/consumersRouter")
+//const storesRouter = require("./routes/storesRouter")
+//const medicinesRouter = require("./routes/medicinesRouter")
 
-const port = 3000;
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
+
+//app.use("/api/users", usersRouter)
+//app.use("/api/pharmacists", pharmacistsRouter)
+//app.use("/api/consumers", consumersRouter)
+//app.use("/api/stores", storesRouter)
+//app.use("/api/medicines", medicinesRouter)
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Express app running on port ${port}`);
