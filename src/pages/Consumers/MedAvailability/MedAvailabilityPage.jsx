74a1b5dd1cc818bb79b5714ee093dabf3f06982c
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-04 23:18:31 +0800

    update store schema to include lat lon and auto calculate lat lon upon updating postal code

--- src/pages/Consumers/MedAvailability/MedAvailabilityPage.jsx
+++ src/pages/Consumers/MedAvailability/MedAvailabilityPage.jsx
@@ -1,11 +1,25 @@
+import { useParams } from "react-router";
 import BotttomNavBar from "../../../components/ConsumerNavBar/BottomNavBar";
 import TopNavBar from "../../../components/ConsumerNavBar/TopNavBar";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
 const MedAvailabilityPage = () => {
+  const { id } = useParams();
   return (
     <>
       <TopNavBar backButton={true} />
-      <h1></h1>
+      <h1>Display Availability in Stores</h1>
+      <MapContainer
+        className="h-[30vh]"
+        center={[1.352638, 103.813529]}
+        zoom={9.5}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
       <BotttomNavBar />
     </>
   );
