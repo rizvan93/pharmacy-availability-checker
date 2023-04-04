c5493a5adc8ed7986ca444cbc6364352187f0abf
ade-t <ad_elyn@hotmail.com>
2023-04-04 21:08:41 +0800

    nav bar

--- src/components/ConsumerNavBar/TopNavBar.jsx
+++ src/components/ConsumerNavBar/TopNavBar.jsx
@@ -1,32 +1,45 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react"
 import userLogo from "../../../src/assets/user.png";
 import infoLogo from "../../../src/assets/info.png";
+import WatsonLogo from "../../../src/assets/WatsonLogo.png";
 
 export default function TopNavBar({ user, backButton }) {
   const navigate = useNavigate();
+  const [dropdown, setDropdown] = useState(false);
 
   const handleBack = () => {
     navigate(-1);
   };
 
+  const handleDropdown= () => {
+    setDropdown(!dropdown); 
+  };
+
   return (
     <div className="bg-white py-3 fixed inset-x-0 top-0">
+
       <div className="container mx-auto flex justify-between items-center">
         {backButton ? (
           <button onClick={handleBack}>BACK</button>
         ) : (
-          <Link to="" className="inline-block text-wAqua ">
             <img src={infoLogo} width="40" />
-          </Link>
         )}
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="inline-block"
-        />
-        <Link to="" className="inline-block text-black ">
-          <img src={userLogo} width="40" />
-        </Link>
+
+        <img src={WatsonLogo} className="inline-block" width="80"/>
+
+       <div >
+        <img src={userLogo} width="40" onClick={handleDropdown}/>
+        {dropdown ? (
+          <ul className="absolute right-0 bg-white border border-gray-200 mt-5">
+          <li><Link to="/login" className="block px-4 py-2 text-wAqua hover:bg-gray-100">Log In</Link></li>
+          <li><Link to="/signup" className="block px-4 py-2 text-wAqua hover:bg-gray-100">Sign Up</Link></li>
+          </ul>
+        ) : null }
+        </div>
+       
+        </div>
       </div>
-    </div>
+  
   );
 }
--- src/pages/Consumers/Index/ConsumersMainPage.jsx
+++ src/pages/Consumers/Index/ConsumersMainPage.jsx
@@ -2,6 +2,7 @@ import BotttomNavBar from "../../../components/ConsumerNavBar/BottomNavBar";
 import TopNavBar from "../../../components/ConsumerNavBar/TopNavBar";
 import WatsonMedi from "../../../../src/assets/WatsonMedi.png";
 import WatsonPharm from "../../../../src/assets/WatsonPharm.png";
+import { Link } from "react-router-dom";
 
 export default function ConsumersMainPage() {
   return (
@@ -10,8 +11,18 @@ export default function ConsumersMainPage() {
       <br />
 
       <div className="container mx-auto p-4 flex flex-col justify-center items-center">
-      <div className="bg-white rounded-lg shadow-md p-4 mb-4"><img src={WatsonMedi} width="800"/></div>
-      <div className="bg-white rounded-lg shadow-md p-4"><img src={WatsonPharm} width="800"/></div>
+      <div className="bg-white rounded-lg shadow-md p-4 mb-4">
+        <Link to="/consumers/pharmacists">
+          <img src={WatsonMedi} width="800"/>
+        </Link>
+      </div>
+
+      <div className="bg-white rounded-lg shadow-md p-4">
+        <Link to="/consumers/medicines">
+          <img src={WatsonPharm} width="800"/>
+        </Link>
+      </div>
+      
       </div>
       <BotttomNavBar />
     </div>
