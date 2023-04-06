1b6d84f18df9f5142c94685f7e6a144afbd61da9
ade-t <ad_elyn@hotmail.com>
2023-04-06 16:06:08 +0800

    sign up css

--- src/pages/Consumers/Create/ConsumerSignUpForm.jsx
+++ src/pages/Consumers/Create/ConsumerSignUpForm.jsx
@@ -35,10 +35,14 @@ export default function ConsumerSignUpForm() {
     setNewUser({ ...newUser, [e.target.name]: e.target.value });
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}
+    className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md">
       <label>
         Name:
-        <input name="name" value={newUser.name} onChange={handleChange} />
+        <input name="name" 
+        value={newUser.name} 
+        onChange={handleChange} 
+        className=" border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2. dark:border-gray-600 dark:text-white"/>
       </label>
       <label>
         Email:
@@ -47,7 +51,7 @@ export default function ConsumerSignUpForm() {
           name="userId"
           value={newUser.userId}
           onChange={handleChange}
-        />
+          className=" border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600"/>
       </label>
       <label>
         Password:
@@ -56,6 +60,7 @@ export default function ConsumerSignUpForm() {
           name="password"
           value={newUser.password}
           onChange={handleChange}
+          className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2. dark:border-gray-600 dark:text-white"
         />
       </label>
       <label>
@@ -63,12 +68,14 @@ export default function ConsumerSignUpForm() {
         <input
           type="password"
           value={confirm}
+          className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2. dark:border-gray-600 dark:text-white"
           onChange={(e) => {
             setConfirm(e.target.value);
           }}
         />
       </label>
-      <button type="submit">Register</button>
+      <br />
+      <button type="submit" className="w-full text-white bg-wAqua font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
     </form>
   );
 }
