599b4caa5f89996a2b015147c83a6a417c581cf1
ade-t <ad_elyn@hotmail.com>
2023-04-06 11:36:16 +0800

    add sign up link

--- src/components/NavBar/LoggedOutOptions.jsx
+++ src/components/NavBar/LoggedOutOptions.jsx
@@ -6,13 +6,13 @@ const LoggedOutOptions = () => {
       <li>
         <Link
           to="/login"
-          className="block px-4 py-2 text-wAqua hover:bg-gray-100"
+          className="block px-3 py-2 text-wAqua hover:bg-gray-100"
         >
           Log In
         </Link>
       </li>
       <li>
-        <Link to="/" className="block px-4 py-2 text-wAqua hover:bg-gray-100">
+        <Link to="/consumers/new" className="block px-3 py-2 text-wAqua hover:bg-gray-100">
           Sign Up
         </Link>
       </li>
