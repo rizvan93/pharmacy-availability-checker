2ac01d40aaf1efdfb85635a6710b07947fa9457a
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-06 13:50:14 +0800

    fix bookmark button on med search page

--- src/pages/Consumers/MedSearch/components/MedCard.jsx
+++ src/pages/Consumers/MedSearch/components/MedCard.jsx
@@ -1,10 +1,13 @@
-const MedCard = ({ medicine, toTitleCase }) => {
+import BookmarkButton from "./BookmarkButton";
+
+const MedCard = ({ medicine, toTitleCase, id }) => {
   return (
     <>
       <hr />
       <h4>{toTitleCase(medicine.name)}</h4>
       <p>{toTitleCase(medicine.manufacturer)}</p>
       <p>{toTitleCase(medicine.quantity)}</p>
+      <BookmarkButton id={id} field="medicines" fieldId={medicine._id} />
     </>
   );
 };
