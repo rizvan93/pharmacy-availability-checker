444ea47e1178d0c5f237bb83f89aec0171a59a0e
ade-t <ad_elyn@hotmail.com>
2023-04-07 14:30:32 +0800

    pharm availability

--- src/pages/Consumers/PharmAvailability/PharmAvailabilityPage.jsx
+++ src/pages/Consumers/PharmAvailability/PharmAvailabilityPage.jsx
@@ -1,12 +1,53 @@
-import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import StoreMap from "../components/StoreMap"
+import StoreCard from "../MedAvailability/StoreCard";
 
 export default function PharmAvailability({ setHome }) {
   useEffect(() => {
     setHome(false);
-  }, []);
+  },[]);
+
+  const { id } = useParams();
+  const [stores, setStores] = useState();
+  const [display, setDisplay] = useState("");
+
+  useEffect(() => {
+    const getStores = async () => {
+      const response = await fetch("/api/stores");
+      const data = await response.json();
+      if (!data.error) {
+        setStores(
+          data.map((s) => {
+            const store = {
+              ...s,
+              stocks: s?.stocks[0],
+            };
+            return store;
+          })
+        );
+      }
+    };
+    getStores();
+
+   
+      const getPharmacists= async () => {
+        const response = await fetch(`/api/pharmacists/${id}`);
+        const data = await response.json();
+        setDisplay(data.name);
+      };
+      getPharmacists();
+    }, [id]);
+
+
   return (
     <>
-      <h1>List of Pharmacists</h1>
+   
+      {display ? <h1>{display}</h1> : null}
+      <StoreMap stores={stores} />
+      {stores?.map((s) => (
+        <StoreCard store={s} key={s._id} pharmacistName={s.pharmacists} />
+      ))}
     </>
-  );
+  )
 }
