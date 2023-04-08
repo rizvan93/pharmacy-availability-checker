b46afc90469f0a4e6b46670c43c72a32cd9733de
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-08 18:21:28 +0800

    read current position in store map. unable to center map at new position

--- src/pages/Consumers/components/StoreMap.jsx
+++ src/pages/Consumers/components/StoreMap.jsx
@@ -1,18 +1,30 @@
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import StoreMarker from "./StoreMarker";
 
-const StoreMap = ({ stores }) => {
+const StoreMap = ({ stores, currentPosition }) => {
+  const [center, setCenter] = useState([1.352638, 103.813529]);
+  const [zoom, setZoom] = useState(9.5);
+
+  useEffect(() => {
+    if (currentPosition) {
+      setCenter([currentPosition.latitude, currentPosition.longitude]);
+      setZoom(15);
+    }
+  }, []);
+
   return (
     <MapContainer
       className="h-[30vh]"
-      center={[1.352638, 103.813529]}
-      zoom={9.5}
+      center={center}
+      zoom={zoom}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       {stores?.map((s) => (
         <StoreMarker store={s} key={s._id} />
       ))}
