664fd77918a3f0131e9a78e90cfac6476867ace4
ade-t <ad_elyn@hotmail.com>
2023-04-08 23:04:23 +0800

    remove duplicate font style

--- src/pages/Consumers/Create/ConsumerSignUpPage.jsx
+++ src/pages/Consumers/Create/ConsumerSignUpPage.jsx
@@ -9,7 +9,7 @@ export default function ConsumerSignUpPage({ setHome }) {
 return (
 <div className="bg-wAqua-10 min-h-screen flex items-center justify-center px-4">
   <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-6 mt-1">
-    <h1 className="text-3xl font-bold text-center mb-5 text-wAqua font-semibold">New User Sign Up</h1>
+    <h1 className="text-3xl text-center mb-5 text-wAqua font-semibold">New User Sign Up</h1>
     <ConsumerSignUpForm />
   </div>
 </div>
