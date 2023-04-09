6b82ee228431fe1565e2d20f2968e94816a998de
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-09 18:23:28 +0800

    fix mapIcons not appearing in cyclic deployed app

--- src/pages/Consumers/components/mapIcons.jsx
+++ src/pages/Consumers/components/mapIcons.jsx
@@ -1,41 +1,49 @@
 import * as L from "leaflet";
+import NoPharmacist__MedicineAvailable from "../../../assets/mapIcons/NoPharmacist__MedicineAvailable.png";
+import NoPharmacist__MedicineLow from "../../../assets/mapIcons/NoPharmacist__MedicineLow.png";
+import NoPharmacist_NoMedicine from "../../../assets/mapIcons/NoPharmacist_NoMedicine.png";
+import Pharmacist_MedicineAvailable from "../../../assets/mapIcons/Pharmacist_MedicineAvailable.png";
+import PharmacistAvailable__MedicineLow from "../../../assets/mapIcons/PharmacistAvailable__MedicineLow.png";
+import PharmacistAvailable__NoMedicine from "../../../assets/mapIcons/PharmacistAvailable__NoMedicine.png";
+import PharmacistAvailable from "../../../assets/mapIcons/PharmacistAvailable.png";
+import PharmacistNotAvailable from "../../../assets/mapIcons/PharmacistNotAvailable.png";
 
 const Icon = L.Icon.extend({
   options: {},
 });
 
 const medPharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/Pharmacist_MedicineAvailable.png",
+  iconUrl: Pharmacist_MedicineAvailable,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
 
 const medNoPharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/NoPharmacist__MedicineAvailable.png",
+  iconUrl: NoPharmacist__MedicineAvailable,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
 
 const lowMedPharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/PharmacistAvailable__MedicineLow.png",
+  iconUrl: PharmacistAvailable__MedicineLow,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
 
 const lowMedNoPharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/NoPharmacist__MedicineLow.png",
+  iconUrl: NoPharmacist__MedicineLow,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
 
 const pharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/PharmacistAvailable.png",
+  iconUrl: PharmacistAvailable,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
 
 const noPharmIcon = new Icon({
-  iconUrl: "/src/assets/mapIcons/PharmacistNotAvailable.png",
+  iconUrl: PharmacistNotAvailable,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
