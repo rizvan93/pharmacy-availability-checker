bbd4e3d6410faf942ca92f551a9060d9f86a24b9
ade-t <ad_elyn@hotmail.com>
2023-04-09 22:09:02 +0800

    remove duplicate hook to clear error message

--- src/pages/User/EditUserPage.jsx
+++ src/pages/User/EditUserPage.jsx
@@ -1,19 +1,6 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import UserEditForm from "./UserEditForm";
 
 export default function EditUserPage() {
-  const { id } = useParams();
-  const [users, setUsers] = useState();
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const response = await fetch(`/api/users/${id}`);
-      const user = await response.json();
-      setUsers(user);
-    };
-    getUsers();
-  }, [id]);
 
   return (
     <div className="mt-20">
