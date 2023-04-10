995bed6851f3de13db63c295adab2866a4ed1d43
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-10 10:08:41 +0800

    update medicines page

--- src/pages/Medicine/Create/AddMedicinePage.jsx
+++ src/pages/Medicine/Create/AddMedicinePage.jsx
@@ -3,7 +3,7 @@ import MedicineForm from "./NewMedicineForm";
 const AddMedicinePage = () => {
   return (
     <>
-      <div className="ml-1 mt-20 px-10">
+      <div className="ml-1">
         <h1 className="mb-4 font-bold">New Medicine</h1>
         <MedicineForm />
       </div>
--- src/pages/Medicine/Index/MedicinesPage.jsx
+++ src/pages/Medicine/Index/MedicinesPage.jsx
@@ -20,16 +20,17 @@ const MedicinesPage = () => {
 
   return (
     <>
-      <div className="mt-12 flex w-screen flex-col items-center justify-center py-6">
-        <div></div>
-        <h1 className="mb-2 font-sans text-3xl font-bold text-wPurple underline">
-          Medicines
-        </h1>
-        <Link to="/medicines/new">
-          <button className=" mb-2 inline-block rounded-xl bg-wAqua px-4 py-1.5 text-white hover:bg-wAqua-50">
-            Add New Medicine
-          </button>
-        </Link>
+      <div className="flex w-screen flex-col items-center justify-center">
+        <div className="flex w-full flex-row justify-between">
+          <h1 className="mb-2 flex-1 font-sans text-3xl font-bold text-wPurple underline">
+            Medicines
+          </h1>
+          <Link to="/medicines/new">
+            <button className=" mb-2 inline-block rounded-xl bg-wAqua px-4 py-1.5 text-white hover:bg-wAqua-50">
+              Add New Medicine
+            </button>
+          </Link>
+        </div>
 
         <MedicinesTable medicines={medicines} removeMedicine={removeMedicine} />
       </div>
